66f6a950c87c6f04bdad9ea2dae79490f7bf9577
Zachary Cabot <cabotzj@vt.edu>
2012-05-02 01:57:44 -0700

    final project commit

---
+++ word_search.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "*************************************************"
+echo "----------Welcome to Word Search 1000!-----------"
+echo "*************************************************"
+
+echo ""
+echo ""
+
+echo "To play game, enter in coordinates for the first and last"
+echo "letter of the word starting from top left corner as 11!"
+
+echo ""
+echo ""
+
+if (( $1 == "help" ) && ( $2 == NULL )); then
+	echo "The proper syntax for the command is: "
+	echo "word_search <1or2>
+
+
+	
+while read line; do
+	echo "${line}"
+done < wordPuzzle
+
+
+
+
+
